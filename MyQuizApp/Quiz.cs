f26f2055397cb30c2fead6a5b89662f7bf367fc4
woozy <woozy@NUC25>
2025-01-30 16:07:27 -0500

    Add project files.

---
+++ MyQuizApp/Quiz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizApp
+{
+    internal class Quiz
+    {
+        private Question[] questions;
+
+        public Quiz(Question[] questions)
+        {
+            this.questions = questions;
+        }
+
+        public void DisplayQuestion(Question question)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(question.QuestionText);
+            Console.ResetColor();
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine($". {question.Answers[i]}");
+            }
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Your answer: ");
+            string input = Console.ReadLine();
+            int choice = 0;
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Invalid choice, please enter a number between 1 and 4.");
+                input = Console.ReadLine();
+            }
+            return choice - 1; // adjust answer to array index based on UI
+        }
+    }
+}
